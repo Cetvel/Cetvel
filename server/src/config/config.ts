29200fb09ca6c4eaf8f8cb60b87cbc55b9f806c0
pysk0101 <kucukaslanmuhammed852@gmail.com>
2024-07-23 16:50:46 +0300

    databased added

---
+++ server/src/config/config.ts
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+const connectDB= async()=> {
+    try {
+        mongoose.set("strictQuery", false);
+        const conn = await mongoose.connect(process.env.MONGO_URI!)
+        console.log(`MongoDB Connected: ${conn.connection.host}`)
+    } catch (error: any) {
+        console.error(`Error: ${error.message}`)       
+    }
+}
+export default connectDB;
