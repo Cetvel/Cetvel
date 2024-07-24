f076ce7d4389c69c3358a55c315fec8fa307bf54
pysk0101 <kucukaslanmuhammed852@gmail.com>
2024-07-24 20:23:18 +0300

    User CRUD added

--- server/src/controllers/user.controllers.ts
+++ server/src/controllers/user.controllers.ts
@@ -2,20 +2,46 @@ import { Request, Response } from 'express';
 import httpStatus from 'http-status';
 import catchAsync from '../utils/catchAsync';
 import UserService from '../services/user.service';
+import ApiError from '../utils/ApiError';
 
-
-const createUser = catchAsync(async (req:Request, res:Response) => {
+const createUser = catchAsync(async (req: Request, res: Response) => {
     const user = await UserService.createUser(req.body);
     res.status(httpStatus.CREATED).send(user);
 });
 
-const getUsersInfo = catchAsync(async (req:Request, res:Response) => {
-    console.log(req.userId);
+const getUser = catchAsync(async (req: Request, res: Response) => {
     const user = await UserService.getUserById(req.userId!);
+    if (!user) {
+        throw new ApiError(httpStatus.NOT_FOUND, 'User not found');
+    }
     res.status(httpStatus.OK).send(user);
 })
 
+
+const updateUser = catchAsync(async (req: Request, res: Response) => {
+    const user = await UserService.updateUserById(req.userId!, req.body);
+    res.send(user);
+});
+
+const updateUserPassword = catchAsync(async (req: Request, res: Response) => {
+    const user = await UserService.updateUserPassword(req.userId!, req.body.currentPassword, req.body.newPassword);
+    res.send(user);
+})
+
+const updateUserEmail = catchAsync(async (req: Request, res: Response) => {
+    const user = await UserService.updateUserEmail(req.userId!, req.body.email, req.body.password);
+    res.send(user);
+});
+const deleteUser = catchAsync(async (req: Request, res: Response) => {
+    await UserService.deleteUserById(req.userId!);
+    res.status(httpStatus.NO_CONTENT).redirect('/');
+});
+
 export default {
     createUser,
-    getUsersInfo
+    getUser,
+    updateUser,
+    deleteUser,
+    updateUserPassword,
+    updateUserEmail
 }
