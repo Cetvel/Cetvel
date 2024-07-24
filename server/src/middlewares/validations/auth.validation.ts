554e6d89ce6166b9ecb50a76cafc9ad9a85d8175
pysk0101 <kucukaslanmuhammed852@gmail.com>
2024-07-24 20:23:52 +0300

    some auth fixes

--- server/src/middlewares/validations/auth.validation.ts
+++ server/src/middlewares/validations/auth.validation.ts
@@ -3,8 +3,9 @@ import { body, ValidationChain } from 'express-validator';
 
 const register: ValidationChain[] = [
   body('email').isEmail().withMessage('Geçerli bir e-posta adresi girin'),
-  body('password').isLength({ min: 6 }).withMessage('Şifre en az 6 karakter olmalıdır'),
-  body('name').notEmpty().withMessage('İsim gereklidir')
+  body('password').isLength({ min: 4 }).withMessage('Şifre en az 4 karakter olmalıdır'),
+  body('name').notEmpty().withMessage('İsim gereklidir'),
+  body('studyField').notEmpty().withMessage('Rol gereklidir').matches(/(YKS|KPSS|LGS)/)
 ];
 
 const login: ValidationChain[] = [
