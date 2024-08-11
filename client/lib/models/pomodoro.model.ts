5cef1d97fc742dc126b738fa00060cc48d3c7c7e
pysk0101 <kucukaslanmuhammed852@gmail.com>
2024-08-11 20:06:23 +0300

    Pomodoro model updated

--- client/lib/models/pomodoro.model.ts
+++ client/lib/models/pomodoro.model.ts
@@ -1,12 +1,12 @@
 import mongoose, { Schema, Model, Document, Types } from "mongoose";
-import { reduceEachLeadingCommentRange } from "typescript";
 
 interface IPomodoro extends Document {
+    clerkId?: string
     title: string;
     tag: string;
-    // description: string;
-    duration: number;
     userId: Types.ObjectId;
+    description: string;
+    duration: number;
 }
 
 export interface PomodoroDocument extends IPomodoro, Document {
@@ -18,13 +18,15 @@ interface PomodoroModel extends Model<PomodoroDocument> { }
 
 
 const PomodoroSchema = new Schema({
+    clerkId: { type: String, required: false },
     title: { type: String, required: true },
     tag: { type: String, required: true },
+    description: { type: String, required: false }, 
     duration: { type: Number, required: true },
-    userId: { type: Types.ObjectId, required: true, ref: 'User' }
+    userId: { type: Types.ObjectId, required: false, ref: 'User' }
 }, { timestamps: true });
 
 
-const Pomodoro = mongoose.model<PomodoroDocument, PomodoroModel>('Pomodoro', PomodoroSchema);
+const Pomodoro = mongoose.models.Pomodoro || mongoose.model<PomodoroDocument, PomodoroModel>('Pomodoro', PomodoroSchema);
 
 export default Pomodoro
