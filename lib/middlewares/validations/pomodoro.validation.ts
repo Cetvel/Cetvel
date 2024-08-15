504f3f6e7a33d2547f47ec0354dc4231a597246c
pysk0101 <kucukaslanmuhammed852@gmail.com>
2024-08-15 09:23:18 +0300

    old files deleted

--- lib/middlewares/validations/pomodoro.validation.ts
+++
@@ -1,9 +0,0 @@
-import { body, ValidationChain } from "express-validator";
-
-const pomodoroValidation : ValidationChain[ ] = [
-    body('title').isString().isLength({min: 3}),
-    body('tag').isString(),
-    body('duration').isNumeric(),
-]
-
-export default pomodoroValidation
