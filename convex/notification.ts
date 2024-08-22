e9037de76eb025f4b087282d6375b8cd083bc1cc
pysk0101 <kucukaslanmuhammed852@gmail.com>
2024-08-22 19:52:49 +0300

    convex notification updated

--- convex/notification.ts
+++ convex/notification.ts
@@ -1,37 +1,15 @@
-import { action, internalAction } from './_generated/server';
+import { query } from './_generated/server';
+import {v} from 'convex/values';
 
-import { v } from 'convex/values';
-import { internal } from './_generated/api';
-
-export const checkAndSendNotifications = action({
-  args: { userId: v.string() },
-  handler: async (ctx, { userId }) => {
-    console.log("Bu action çalışıyor, kafa relax olsun.");
-    try {
-      //
-      console.log("Sending notification to user on Actions:", userId);
-      const response = await fetch(`https://bfd1-78-191-178-225.ngrok-free.app/api/notification?userId=${userId}`);
-      console.log("Response status:", response.status);
-      console.log("Response headers:", response.headers);
-      if (!response.ok) {
-        const errorText = await response.text();
-        console.error("API Error response:", errorText); // Debug log
-        throw new Error(`API request failed with status ${response.status}: ${errorText}`);
-      }
-
-      const todoCounts = await response.json()
-      console.log("todocounts" , todoCounts);
-      if (todoCounts.count > 0) {
-        const count = todoCounts.count;
-        const message = `Bugün bitirilmesi gereken ${count} göreviniz var.`;
-        await ctx.runMutation(internal.todoReminder.createTodoReminderNoti , { userId, message });
-        return { success: true, message };
-      } else {
-        return { success: true, message: "Bildirim gönderilecek görev bulunamadı." };
-      }
-    } catch (error) {
-      console.error("Hata oluştu:", error);
-      return { success: false, message: "Bir hata oluştu YINE VE YENIDEN." };
-    }
+export const getNotifications = query({
+  args: {
+      clerkId: v.string(),
+  },
+  handler: async (ctx, { clerkId }) => {
+      const notifications = await ctx.db
+          .query("notification")
+          .filter((q) => q.eq(q.field("userId"), clerkId))
+          .collect()
+      return notifications;
   },
 });
