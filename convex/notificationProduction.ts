0e817b82cc269217c5b74ba254352d058cd9c042
pysk0101 <kucukaslanmuhammed852@gmail.com>
2024-08-22 19:54:05 +0300

    notification Production Center added

---
+++ convex/notificationProduction.ts
@@ -0,0 +1,31 @@
+import { internal } from "./_generated/api";
+import { internalAction, internalQuery } from "./_generated/server";
+
+
+export const produceNotifications = internalAction({
+    handler: async (ctx) => {
+        const allPreferences = await ctx.runQuery(internal.notificationProduction.getAllPreferences);
+        console.log(allPreferences);
+        const currentTime = Date.now();
+
+        for (const userPref of allPreferences) {
+            if (userPref.todoReminder) {
+                await ctx.runAction(internal.todoReminder.checkAndProduceTodoReminder, {
+                    userId: userPref.clerkId,
+                    currentTime,
+                    frequency : userPref.todoReminderFrequency,
+                    lastReminder : userPref.lastTodoReminder
+                })
+            }
+            // if (userPref.pomodoroReminder) {
+            //     await ctx.runMutation(internal.pomodoroReminder.checkPomodoroStatus);
+            // }
+        }
+    },
+});
+
+export const getAllPreferences = internalQuery({
+    handler: async (ctx) => {
+        return ctx.db.query("userPreferences").collect()
+    }
+})
