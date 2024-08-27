602072453af4b725e412aa6efd2f189962e8e4e4
pysk0101 <kucukaslanmuhammed852@gmail.com>
2024-08-27 21:09:09 +0300

    Dgs model and crud  added

---
+++ lib/models/exam-models/dgs.model.ts
@@ -0,0 +1,46 @@
+import mongoose, { Schema, model, Document } from 'mongoose'
+import Exam from '../exam.model'
+
+
+export interface IDGS {
+    solvingTime?: number,
+    math: {
+        solvingTime?: number,
+        correct: number,
+        wrong: number,
+        empty: number,
+    },
+    turkish: {
+        solvingTime?: number,
+        correct: number,
+        wrong: number,
+        empty: number,
+    }
+}
+
+
+export interface IDGSDocument extends IDGS, Document {
+    _id: Schema.Types.ObjectId;
+}
+const DgsSchema = new Schema<IDGSDocument>({
+    solvingTime: { type: Number, required: false, max: 135 },
+    math: { 
+        solvingTime: { type: Number, required: false, max: 135 },
+        correct: { type: Number, required: true, min: 0, max: 40 },
+        wrong: { type: Number, required: true, min: 0, max: 40 },
+        empty: { type: Number, required: true, min: 0, max: 40 }
+    },
+    turkish: {
+        solvingTime: { type: Number, required: false, max: 135 },
+        correct: { type: Number, required: true, min: 0, max: 40 },
+        wrong: { type: Number, required: true, min: 0, max: 40 },
+        empty: { type: Number, required: true, min: 0, max: 40 }
+    },
+});
+
+
+const Dgs = (mongoose.models.Dgs as mongoose.Model<IDGSDocument>) || 
+    Exam.discriminator<IDGSDocument>('Dgs', DgsSchema);
+
+export default Dgs;
+
