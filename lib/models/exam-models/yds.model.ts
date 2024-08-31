8088f8f6fe0205f38bc072cabd114908f57ed663
pysk0101 <kucukaslanmuhammed852@gmail.com>
2024-08-31 09:47:07 +0300

    models updated

--- lib/models/exam-models/yds.model.ts
+++ lib/models/exam-models/yds.model.ts
@@ -1,14 +1,12 @@
 import mongoose, { Schema, model, Document } from 'mongoose'
 import Exam from '../exam.model'
-import { PhoneNumber } from '@clerk/nextjs/server';
-
 
 export interface IYDS{
     solvingTime?: number,
-    english: { 
-            solvingTime: { type: Number, required: false, max: 135 },
-            correct: { type: Number, required: true, min: 0, max: 40 },
-            wrong: { type: Number, required: true, min: 0, max: 40 }
+    foreingLanguage: { 
+            solvingTime: { type: Number, required: false, max: 180 },
+            correct: { type: Number, required: true, min: 0, max: 80 },
+            wrong: { type: Number, required: true, min: 0, max: 80 }
     },
     point: number
 }
@@ -18,11 +16,11 @@ export interface IYDSDocument extends IYDS, Document {
     _id: Schema.Types.ObjectId;
 }
 const YdsSchema = new Schema<IYDSDocument>({
-    solvingTime: { type: Number, required: false, max: 135 },
-    english: { 
-        solvingTime: { type: Number, required: false, max: 135 },
-        correct: { type: Number, required: true, min: 0, max: 40 },
-        wrong: { type: Number, required: true, min: 0, max: 40 }
+    solvingTime: { type: Number, required: false, max: 180 },
+    foreingLanguage: { 
+        solvingTime: { type: Number, required: false, max: 180 },
+        correct: { type: Number, required: true, min: 0, max: 80 },
+        wrong: { type: Number, required: true, min: 0, max: 80 }
     },
     point: Number 
 });
--- lib/models/plugins/tyt.plugins.ts
+++ lib/models/plugins/tyt.plugins.ts
@@ -1,26 +1,26 @@
-import { TytDocument,  Section } from "../exam-models/tyt.model";
+import { ITytDocument,  Section } from "../exam-models/tyt.model";
 
 export interface ITytMethods {
     calculateSectionNet(section: keyof Section): number;
     analyzeSectionPerformance(section: keyof Section): object;
-    calculateAnswerStats(objec: TytDocument): { correctAnswers: number, wrongAnswers: number, empty: number }
+    calculateAnswerStats(objec: ITytDocument): { correctAnswers: number, wrongAnswers: number, empty: number }
 }
 
 
 
 export const TytMethods = {
-    calculateSectionNet: function (this: TytDocument, section: keyof Section): number {
+    calculateSectionNet: function (this: ITytDocument, section: keyof Section): number {
         const sectionData = this[section];
         return sectionData.correct - (sectionData.wrong * 0.25);
     },
-    calculateAnswerStats: function (this: TytDocument): { correctAnswers: number, wrongAnswers: number, empty: number } {
+    calculateAnswerStats: function (this: ITytDocument): { correctAnswers: number, wrongAnswers: number, empty: number } {
         const { math, science, turkish, social } = this;
         const correctAnswers = math.correct + science.correct + turkish.correct + social.correct;
         const wrongAnswers = math.wrong + science.wrong + turkish.wrong + social.wrong;
         const empty = math.empty + science.empty + turkish.empty + social.empty;
         return { correctAnswers, wrongAnswers, empty };
     },
-    analyzeSectionPerformance: function (this: TytDocument, section: keyof Section): object {
+    analyzeSectionPerformance: function (this: ITytDocument, section: keyof Section): object {
         const sectionData = this[section];
         const totalQuestions = sectionData.correct + sectionData.wrong + sectionData.empty;
         return {
