c37ad9aad4bbc16321c76ff64404fdbd48a64ae4
pysk0101 <kucukaslanmuhammed852@gmail.com>
2024-09-01 16:42:15 +0300

    userPrefeerence updated added

---
+++ app/api/settings/userPreference/route.ts
@@ -0,0 +1,22 @@
+import { NextResponse, NextRequest } from "next/server";
+import { ConvexHttpClient } from 'convex/browser';
+import { api } from "@/convex/_generated/api";
+import { getAuth } from "@clerk/nextjs/server";
+const convex = new ConvexHttpClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+
+
+export async function PUT(req: NextRequest, res: NextResponse) {
+    try {
+        const clerkId =  getAuth(req).userId
+        if(!clerkId) return NextResponse.json({error:"unauthorized"}, {status: 401})
+        
+        await convex.mutation(
+            api.user.userPreference.updateUserPreference, 
+            {clerkId, props: req.body}
+        )
+        return NextResponse.json({message:"Kullanıcı tercihleri güncellendi."}, {status: 200})
+    } catch (error:any) {
+        console.error(error)
+        return NextResponse.error()
+    }
+}
