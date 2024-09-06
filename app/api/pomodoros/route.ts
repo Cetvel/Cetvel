129bd76734357e97f92383b79bb4ebc4dd406a16
pysk0101 <kucukaslanmuhammed852@gmail.com>
2024-09-06 18:37:43 +0300

    pomodoro route udpawted

--- app/api/pomodoros/route.ts
+++ app/api/pomodoros/route.ts
@@ -3,11 +3,7 @@ import { NextResponse } from "next/server";
 import { getAuth } from "@clerk/nextjs/server";
 import PomodoroModel from "@/lib/models/pomodoro.model";
 import { PomodoroDocument } from "@/lib/models/pomodoro.model";
-<<<<<<< HEAD
 import connectDB from "@/lib/config/connectDB";
-=======
-import connectDB from '@/lib/config/connectDB';
->>>>>>> 17fb4ad34c501c0b34d11b6fa101444dbef8945a
 
 
 export async function GET(request: NextRequest) {
@@ -17,30 +13,12 @@ export async function GET(request: NextRequest) {
   
     try {
       await connectDB();
-<<<<<<< HEAD
       const pomodoros = await PomodoroModel.find({ clerkId: getAuth(request).userId });
       return NextResponse.json(pomodoros, {
         headers: {
           "X-Cache-Status": "MISS",
         },
       });
-=======
-      const totalPomodoros = await PomodoroModel.countDocuments({ clerkId: getAuth(request).userId! });
-  
-      const pomodoros = await PomodoroModel.find({ clerkId: getAuth(request).userId! })
-        .skip(skip)
-        .limit(limit)
-        .sort({ createdAt: -1 });
-  
-      const totalPages = Math.ceil(totalPomodoros / limit);
-  
-      return NextResponse.json({
-        pomodoros,
-        currentPage: page,
-        totalPages,
-        totalPomodoros,
-      },{status:200});
->>>>>>> 17fb4ad34c501c0b34d11b6fa101444dbef8945a
     } catch (error) {
       return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
     }
@@ -59,6 +37,7 @@ export async function POST(request: NextRequest) {
         if (!body) {
             return NextResponse.json({ error: "Request body is empty" }, { status: 400 });
         }
+        
         await connectDB();
         const pomodoro = new PomodoroModel({
             clerkId: getAuth(request).userId,
