f43b27e444bbbccdaa7cc469e45e7d51109052d0
pysk0101 <kucukaslanmuhammed852@gmail.com>
2024-09-11 18:48:09 +0300

    aytFeild added

--- lib/models/user.model.ts
+++ lib/models/user.model.ts
@@ -9,14 +9,23 @@ enum StudyField {
     DGS
 }
 
+enum aytField {
+    SAY,
+    SOZ,
+    EA,
+    DIL
+}
 interface IUser {
     clerkId: string;
     name: string;
     email: string;
     password: string;
+    aytField?: aytField;
     class?: number;
     studyField?: string;
 }
+
+
 
 export interface IUserDocument extends IUser, Document, IUserMethods {
     _id: Schema.Types.ObjectId;
@@ -47,7 +56,12 @@ const userSchema = new Schema<IUserDocument, UserModel>({
         enum: Object.values(StudyField),
         trim: false,
     },
-
+    aytField: {
+        type: String,
+        required: false,
+        enum: Object.values(aytField),
+        trim: false,
+    },
     email: {
         type: String,
         required: true,
