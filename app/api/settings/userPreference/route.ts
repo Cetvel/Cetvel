b65b6fd3445f3dfa55b90c98f13b37d0b8a45ec3
pysk0101 <kucukaslanmuhammed852@gmail.com>
2024-09-13 20:05:22 +0300

    user preference route updated

--- app/api/settings/userPreference/route.ts
+++ app/api/settings/userPreference/route.ts
@@ -1,8 +1,12 @@
 import { NextResponse, NextRequest } from "next/server";
 import { ConvexHttpClient } from 'convex/browser';
 import { api } from "@/convex/_generated/api";
-import { getAuth } from "@clerk/nextjs/server";
+import { getAuth, clerkClient } from "@clerk/nextjs/server";
+import User from '@/lib/models/user.model';
+import connectDB from '@/lib/config/connectDB';
+
 const convex = new ConvexHttpClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+
 
 
 export async function PUT(req: NextRequest, res: NextResponse) {
@@ -10,9 +14,39 @@ export async function PUT(req: NextRequest, res: NextResponse) {
         const clerkId = getAuth(req).userId
         if (!clerkId) return NextResponse.json({ error: "unauthorized" }, { status: 401 })
 
+        const {
+            field, // ayt alani
+            grade,// sinifi sayi cinsinden 
+            notifications, // booelan
+            todoReminders, // boolean
+            todoReminderFrequencyHours // number  
+        } = await req.json()
+
+        const props = {
+            todoReminderFrequencyHours,
+            todoReminders,
+            notifications
+        }
+
+        await clerkClient().users.updateUser(clerkId, {
+            publicMetadata: {
+                field,
+                grade,
+                notifications,
+            },
+        });
+
+        await connectDB()
+        const user = await User.findOneAndUpdate(
+            { clerkId },
+            { field, grade },
+            { new: true }
+        );
+        if (!user) return NextResponse.json({ error: 'User not found' }, { status: 404 });
+
         await convex.mutation(
             api.user.userPreference.updateUserPreference,
-            { clerkId, props: req.body }
+            { clerkId, props }
         )
         return NextResponse.json({ status: 200 })
     } catch (error: any) {
