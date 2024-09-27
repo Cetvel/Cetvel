b5984403c9f852c0245e95e04be1a85b2bc7f042
pysk0101 <kucukaslanmuhammed852@gmail.com>
2024-09-27 18:49:59 +0300

    EXMP2 PLS VERCEL IGNORE ME BRO

--- app/api/exams/[id]/route.ts
+++ app/api/exams/[id]/route.ts
@@ -9,8 +9,8 @@ export async function PUT(request: NextRequest, { params }: { params: { id: stri
         if (!userId) {
             return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
         }
-// asdfasdfasdf
-        const { id } = params;//PKZOASD]OKASDKASDPKASDFPASDFPOASDFASDFASDSDPFOKasdfasdfasdfasdf
+
+        const { id } = params;//PKZOASD]OKASDKASDPKASDFPASDFPOASDFASDFASDSDPFOK
         if (!id) {
             return NextResponse.json({ error: "Request paramater is empty" }, { status: 400 });
         }
