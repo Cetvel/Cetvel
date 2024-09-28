471bf456607a7085987aa539c2d3a88f33ed5cd7
Cetvel <cetvelapp@gmail.com>
2024-09-28 12:38:27 +0300

    bug fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,18 +38,6 @@ export default function RootLayout({
         suppressHydrationWarning
       >
         <head>
-          {/* Google Tag Manager */}
-          <Script id='gtm'>
-            {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-PWRBCK5M');
-          `}
-          </Script>
-          {/* End Google Tag Manager */}
-
           <Script
             async
             src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
@@ -60,21 +48,11 @@ export default function RootLayout({
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', ${'${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}'});
+              gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS});
           `}
           </Script>
         </head>
         <body className={dm_sans.className}>
-          {/* Google Tag Manager (noscript) */}
-          <noscript>
-            <iframe
-              src='https://www.googletagmanager.com/ns.html?id=GTM-PWRBCK5M'
-              height='0'
-              width='0'
-              style={{ display: 'none', visibility: 'hidden' }}
-            ></iframe>
-          </noscript>
-          {/* End Google Tag Manager (noscript) */}
           <ConvexClientProvider>
             <ThemeProvider
               attribute='class'
