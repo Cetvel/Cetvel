1d237f4593dfe07afa57fb77ba50c4a023383eba
mr_isik <faruk_isik_24@protonmail.com>
2024-09-28 10:40:37 +0300

    google analytics added

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,7 +40,7 @@ export default function RootLayout({
         <head>
           <Script
             async
-            src='https://www.googletagmanager.com/gtag/js?id=G-E7NY2W59JZ'
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
           />
 
           <Script id='google-analytics'>
