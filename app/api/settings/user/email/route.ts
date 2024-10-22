d01675d55be8c0d1ca774037f784314a12b59417
pysk0101 <kucukaslanmuhammed852@gmail.com>
2024-10-22 11:49:09 +0300

    update: email settings endpoint

--- app/api/settings/user/email/route.ts
+++ app/api/settings/user/email/route.ts
@@ -2,11 +2,12 @@ import { NextRequest, NextResponse } from 'next/server';
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 import getM2MToken from '@/lib/m2m_token';
 import { Users, init } from "@kinde/management-api-js";
-import UserMongo from '@/lib/models/user.model';
+import Email from '@/lib/models/email.model';
+import connectDB from '@/lib/config/connectDB';
 
 init();
 
-export async function PUT(req: NextRequest) {
+export async function POST(req: NextRequest) {
     try {
         const user = await getKindeServerSession().getUser();
         if (!user) {
@@ -22,18 +23,22 @@ export async function PUT(req: NextRequest) {
                 type: "email"
             }
         })
-
+        await connectDB()
+        await Email.create({
+            kindeId: user.id,
+            value: email,
+            isPrimary: false
+        }).then(() => console.log('Email created'));
         return NextResponse.json({ status: 200 });
+
     } catch (error: any) {
         console.error('İşlem hatası:', error);
-        console.log("error request", error.request)
         if (error.request.errors["429"] == 'Request was throttled.') {
             return NextResponse.json({ message: "Bu mail önceden eklenmiş." }, { status: 429 });
         }
         return NextResponse.json({ message: "Beklenmedik Sunucu Hatası" }, { status: 500 });
     }
 }
-
 
 export async function DELETE(req: NextRequest) {
     try {
@@ -66,6 +71,12 @@ export async function DELETE(req: NextRequest) {
                     Authorization: `Bearer ${accessToken}`,
                 }
             })
+        await connectDB()
+
+        await Email.deleteOne({
+            kindeId: user.id,
+            value: email
+        }).then(() => console.log('Email deleted'));
 
         return NextResponse.json({ status: 200 });
     } catch (error) {
@@ -74,8 +85,54 @@ export async function DELETE(req: NextRequest) {
     }
 }
 
-function getIdentityId(data: any, email: string): string | undefined {
-    return data.identities.find((item: any) =>
-        item.name === email
-    )?.id;
+export async function PUT(req: NextRequest) {
+    try {
+        const user = await getKindeServerSession().getUser();
+        if (!user) {
+            console.warn('Yetkilendirme Hatası PUT request: User not authenticated');
+            return NextResponse.json({ message: 'Yetkilendirme Hatası' }, { status: 401 });
+        }
+        const { email } = await req.json();
+
+        const data = await Users.getUserIdentities({
+            userId: user?.id
+        }) as any
+        const emails = data.identities
+            .filter((item: any) => item.type === "email")
+
+        const emailId = emails.find((item: any) => item.name == email).id
+        const accessToken = await getM2MToken();
+
+
+        await fetch(`https://cetvel.kinde.com/api/v1/identities/${emailId}`,
+            {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json',
+                    'Authorization': `Bearer ${accessToken}`
+                },
+                body: JSON.stringify({
+                    is_primary: true
+                })
+            })
+
+
+        await connectDB()
+        await Email.updateMany(
+            { kindeId: user.id },
+            { primary: false }
+        );
+        await Email.findOneAndUpdate({
+            kindeId: user.id,
+            value: email
+        }, {
+            isPrimary: true
+        }).then(() => console.log('Email updated'));
+
+        return NextResponse.json({ status: 200 });
+    } catch (error) {
+        console.error('İşlem hatası:', error);
+        return NextResponse.json({ message: "Beklenmedik Sunucu Hatası" }, { status: 500 });
+    }
 }
