6ceefd8cf2ccb5a80d480e7314f0fd22ca098757
pysk0101 <kucukaslanmuhammed852@gmail.com>
2024-10-22 11:48:20 +0300

    update: user setttings

--- app/api/settings/user/route.ts
+++ app/api/settings/user/route.ts
@@ -2,6 +2,9 @@ import { NextRequest, NextResponse } from 'next/server';
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 import getM2MToken from '@/lib/m2m_token';
 import { Users, init } from "@kinde/management-api-js";
+import UserMongo from '@/lib/models/user.model';
+init();
+
 init();
 
 export async function PUT(req: NextRequest) {
@@ -10,29 +13,55 @@ export async function PUT(req: NextRequest) {
         const user = await getUser();
 
         if (!user) {
-            console.warn('Yetkilendirme Hatası PUT request: User not authenticated');
+            console.warn('PUT request: User not authenticated');
             return NextResponse.json({ message: 'Yetkilendirme Hatası' }, { status: 401 });
         }
-        //    requestBody={username,picture}
-        const requestBody = await req.json();
 
-        const accessToken = await getM2MToken();
-        const response = await fetch(`https://cetvel.kinde.com/api/v1/user?id=${user.id}`, {
-            method: 'PATCH',
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json',
-                Authorization: `Bearer ${accessToken}`,
-            },
-            body: JSON.stringify(requestBody)
+        const { username, given_name, family_name } = await req.json();
+        await Users.updateUser({
+            id: user.id,
+            requestBody: {
+                given_name,
+                family_name
+            }
+        })
+        
+        await Users.createUserIdentity({
+            userId: user.id,
+            requestBody: {
+                value: username,
+                type: "username"
+            }
         })
 
-        if (!response.ok) {
-            console.error('Kinde API Hatası:', response.statusText);
-            return NextResponse.json({ message: "Kinde API Hatası" }, { status: 500 });
-        }
-        
         return NextResponse.json({ status: 200 });
+    } catch (error) {
+        console.error('İşlem hatası:', error);
+        return NextResponse.json({ message: "Beklenmedik Sunucu Hatası" }, { status: 500 });
+    }
+}
+
+
+export async function DELETE(req: NextRequest) {
+    try {
+        const { getUser } = getKindeServerSession();
+        const user = await getUser();
+
+        if (!user) {
+            console.warn('Yetkilendirme Hatası DELETE request: User not authenticated');
+            return NextResponse.json({ message: 'Yetkilendirme Hatası' }, { status: 401 });
+        }
+
+        const response = await Users.deleteUser({
+            id: user.id,
+            isDeleteProfile: true
+        })
+        if (response.code === "OK") {
+            return NextResponse.json({ status: 200 });
+        } else {
+            console.error('Kinde API Hatası:', response.message);
+            return NextResponse.json({ message: "Beklenmedik Sunucu Hatası" }, { status: 500 });
+        }
     } catch (error) {
         console.error('İşlem hatası:', error);
         return NextResponse.json({ message: "Beklenmedik Sunucu Hatası" }, { status: 500 });
