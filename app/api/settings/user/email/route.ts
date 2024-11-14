74165a0f146f2d139bb16e29da083f564d62f5b2
pysk0101 <kucukaslanmuhammed852@gmail.com>
2024-11-14 23:13:44 +0300

    update: changing email system

--- app/api/settings/user/email/route.ts
+++ app/api/settings/user/email/route.ts
@@ -3,7 +3,6 @@ import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 import getM2MToken from '@/lib/m2m_token';
 import { Users, init } from '@kinde/management-api-js';
 import User from '@/features/users/models/user.model';
-   
 init();
 export async function PUT(req: NextRequest) {
   try {
@@ -15,37 +14,38 @@ export async function PUT(req: NextRequest) {
         { status: 401 }
       );
     }
+    console.log("user", user);
     const { email } = await req.json();
-    await Users.createUserIdentity({
-      userId: user?.id,
-      requestBody: {
-        value: email,
-        type: 'email',
-      },
-    });
 
     const data = (await Users.getUserIdentities({
       userId: user?.id,
     })) as any;
     console.log(data);
-    const emailId = data.identities
-      .filter((item: any) => item.type === 'email')
-      .find((item: any) => item.name == user.email).id;
+    const response = await Users.createUserIdentity({
+      userId: user.id,
+      requestBody: {
+        value: email,
+        type: 'email',
+      },
+    })
+    const identityId = response.identity?.id;
     const accessToken = await getM2MToken();
-    await fetch(`https://cetvel.kinde.com/api/v1/identities/${emailId}`, {
-      method: 'DELETE',
+    await fetch(`https://cetvel.kinde.com/api/v1/identities/${identityId}`, {
+      method: 'PATCH',
       headers: {
-        'Content-Type': 'application/json',
         Accept: 'application/json',
+        'Content-Type': 'application/json',
         Authorization: `Bearer ${accessToken}`,
       },
-    });
-       ;
+      body: JSON.stringify({
+        is_primary: true,
+      })
+    })
     await User.findOneAndUpdate({ kindeId: user.id }, { email: email });
     return NextResponse.json({ status: 200 });
   } catch (error: any) {
     console.error('İşlem hatası:', error);
-    if (error.request.errors['429'] == 'Request was throttled.') {
+    if (error?.request?.errors && error.request.errors['429'] === 'Request was throttled.') {
       return NextResponse.json(
         { message: 'Bu e-posta adresi önceden eklenmiş.' },
         { status: 429 }
