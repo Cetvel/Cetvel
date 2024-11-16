9b0f0b512920fdf74ca31f10acc413d3dafc203b
pysk0101 <kucukaslanmuhammed852@gmail.com>
2024-11-16 14:26:50 +0300

    fix: POST func first argumant

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,9 +1,9 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import Contact from '@/lib/models/contact.model';
 import connectDB from '@/lib/config/connectDB';
 
 /// some change
-export async function POST(res: NextResponse) {
+export async function POST(res : NextRequest) {
   try {
     await connectDB();
     const { name, email, message } = await res.json();
